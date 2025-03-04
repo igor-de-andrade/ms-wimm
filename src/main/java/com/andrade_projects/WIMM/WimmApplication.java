17627086d7d8171c5f4fcfdb8898afdf152e07d6
igor-de-andrade <cadastros.igordeandrade@gmail.com>
2025-03-04 15:59:43 -0300

    First commit with project creation

---
+++ src/main/java/com/andrade_projects/WIMM/WimmApplication.java
@@ -0,0 +1,13 @@
+package com.andrade_projects.WIMM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WimmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WimmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrade_projects/WIMM/WimmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrade_projects.WIMM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WimmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
